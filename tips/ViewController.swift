0c7539931ec5e25b8fceca598f88b0e50c6b09f3
Ashkhen Sargsyan <ashi@a5.io>
2016-01-17 13:10:56 -0800

    - Added launchsreen - Added a karma bank feature - Cleaned up UI

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,12 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var karmaPointsLabel: UILabel!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    let preferences = NSUserDefaults.standardUserDefaults()
+    var karmaPoints = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "0.00"
         totalLabel.text = "0.00"
+        karmaPoints = preferences.integerForKey("Karma")
+        karmaPointsLabel.text = String(karmaPoints)
+        billField.becomeFirstResponder()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,21 +36,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onEditingChanged(sender: AnyObject) {
+    @IBAction func confirmButtonClick(sender: AnyObject) {
+        if billField != "" {
+            preferences.setInteger(karmaPoints, forKey: "Karma")
+            karmaPointsLabel.text = String(karmaPoints)
+            karmaPointsLabel.frame = CGRectMake(300, 300, 0, 300)
+            UIView.animateWithDuration(0.8, animations: { () -> Void in
+                self.karmaPointsLabel.frame = CGRectMake(200, 10, 50, 100)
+            })
+            preferences.synchronize()
+            view.endEditing(true)
+        }
         
+    }
+    
+    @IBAction func billFieldEditingChanged(sender: AnyObject) {
+        if billField.text != "" {
+            confirmButton.enabled = true
+            calcTip()
+        } else {
+            confirmButton.enabled = false
+        }
+    }
+    
+    @IBAction func tipControlChanged(sender: AnyObject) {
+        calcTip()
+    }
+    
+    func calcTip() {
         let tipPercentages = [0.18, 0.2, 0.25]
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        if tipPercentage == 0.18 {
+            karmaPoints += 1
+        } else if tipPercentage == 0.2 {
+            karmaPoints += 5
+        } else {
+            karmaPoints += 10
+        }
         
         if let billFieldString = billField.text, billAmount = Double(billFieldString) {
             let tip = billAmount * tipPercentage
             let total = billAmount + tip
             
-            tipLabel.text = String(format: "$%f",tip)
+            tipLabel.text = String(format: "$%.2f",tip)
             totalLabel.text = String(format: "$%.2f", total)
         }
     }
-    
-    @IBAction func OnTap(sender: AnyObject) {
+    @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
 }
